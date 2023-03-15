c6bbaebd308825e1479c1418990f341b5e77b4a6
spacedub <spaceraccoon@codecomet.io>
2023-03-15 14:39:44 -0700

    Lint

--- builder/commands/workers.go
+++ builder/commands/workers.go
@@ -15,86 +15,108 @@ import (
 	"github.com/tonistiigi/units"
 )
 
-func Workers(node *builder.Node, w io.Writer, ctx context.Context, verbose bool, format string, filter []string) error {
-	c, err := getClient(node)
+const defaultTabWidth = 8
+
+func Workers(ctx context.Context, node *builder.Node, writer io.Writer, verbose bool,
+	format string, filter []string) error {
+	cli, err := getClient(node) //nolint:contextcheck
+
 	if err != nil {
 		return err
 	}
-	workers, err := c.ListWorkers(ctx, client.WithFilter(filter))
+
+	workers, err := cli.ListWorkers(ctx, client.WithFilter(filter))
+
 	if err != nil {
 		return err
 	}
+
 	if format != "" {
 		tmpl, err := parseTemplate(format)
+
 		if err != nil {
 			return err
 		}
-		if err := tmpl.Execute(w, workers); err != nil {
+
+		if err := tmpl.Execute(writer, workers); err != nil {
 			return err
 		}
-		_, err = fmt.Fprintf(w, "\n")
+
+		_, err = fmt.Fprintf(writer, "\n")
+
 		return err
 	}
 
-	tw := tabwriter.NewWriter(w, 1, 8, 1, '\t', 0)
+	tabWriter := tabwriter.NewWriter(writer, 1, defaultTabWidth, 1, '\t', 0)
 
 	if verbose {
-		printWorkersVerbose(tw, workers)
+		printWorkersVerbose(tabWriter, workers)
 	} else {
-		printWorkersTable(tw, workers)
+		printWorkersTable(tabWriter, workers)
 	}
+
 	return nil
 }
 
-func printWorkersVerbose(tw *tabwriter.Writer, winfo []*client.WorkerInfo) {
-	for _, wi := range winfo {
-		fmt.Fprintf(tw, "ID:\t%s\n", wi.ID)
-		fmt.Fprintf(tw, "Platforms:\t%s\n", joinPlatforms(wi.Platforms))
-		fmt.Fprintf(tw, "BuildKit:\t%s %s %s\n", wi.BuildkitVersion.Package, wi.BuildkitVersion.Version, wi.BuildkitVersion.Revision)
-		fmt.Fprintf(tw, "Labels:\n")
-		for _, k := range sortedKeys(wi.Labels) {
-			v := wi.Labels[k]
-			fmt.Fprintf(tw, "\t%s:\t%s\n", k, v)
+func printWorkersVerbose(tabWriter *tabwriter.Writer, winfo []*client.WorkerInfo) {
+	for _, workerInfo := range winfo {
+		fmt.Fprintf(tabWriter, "ID:\t%s\n", workerInfo.ID)
+		fmt.Fprintf(tabWriter, "Platforms:\t%s\n", joinPlatforms(workerInfo.Platforms))
+		fmt.Fprintf(tabWriter, "BuildKit:\t%s %s %s\n", workerInfo.BuildkitVersion.Package,
+			workerInfo.BuildkitVersion.Version, workerInfo.BuildkitVersion.Revision)
+		fmt.Fprintf(tabWriter, "Labels:\n")
+
+		for _, k := range sortedKeys(workerInfo.Labels) {
+			v := workerInfo.Labels[k]
+			fmt.Fprintf(tabWriter, "\t%s:\t%s\n", k, v)
 		}
-		for i, rule := range wi.GCPolicy {
-			fmt.Fprintf(tw, "GC Policy rule#%d:\n", i)
-			fmt.Fprintf(tw, "\tAll:\t%v\n", rule.All)
+
+		for i, rule := range workerInfo.GCPolicy {
+			fmt.Fprintf(tabWriter, "GC Policy rule#%d:\n", i)
+			fmt.Fprintf(tabWriter, "\tAll:\t%v\n", rule.All)
+
 			if len(rule.Filter) > 0 {
-				fmt.Fprintf(tw, "\tFilters:\t%s\n", strings.Join(rule.Filter, " "))
+				fmt.Fprintf(tabWriter, "\tFilters:\t%s\n", strings.Join(rule.Filter, " "))
 			}
+
 			if rule.KeepDuration > 0 {
-				fmt.Fprintf(tw, "\tKeep Duration:\t%v\n", rule.KeepDuration.String())
+				fmt.Fprintf(tabWriter, "\tKeep Duration:\t%v\n", rule.KeepDuration.String())
 			}
+
 			if rule.KeepBytes > 0 {
-				fmt.Fprintf(tw, "\tKeep Bytes:\t%g\n", units.Bytes(rule.KeepBytes))
+				fmt.Fprintf(tabWriter, "\tKeep Bytes:\t%g\n", units.Bytes(rule.KeepBytes))
 			}
 		}
-		fmt.Fprintf(tw, "\n")
+
+		fmt.Fprintf(tabWriter, "\n")
 	}
 
-	tw.Flush()
+	tabWriter.Flush()
 }
 
-func printWorkersTable(tw *tabwriter.Writer, winfo []*client.WorkerInfo) {
-	fmt.Fprintln(tw, "ID\tPLATFORMS")
+func printWorkersTable(tabWriter *tabwriter.Writer, winfo []*client.WorkerInfo) {
+	fmt.Fprintln(tabWriter, "ID\tPLATFORMS")
 
-	for _, wi := range winfo {
-		id := wi.ID
-		fmt.Fprintf(tw, "%s\t%s\n", id, joinPlatforms(wi.Platforms))
+	for _, workerInfo := range winfo {
+		id := workerInfo.ID
+		fmt.Fprintf(tabWriter, "%s\t%s\n", id, joinPlatforms(workerInfo.Platforms))
 	}
 
-	tw.Flush()
+	tabWriter.Flush()
 }
 
 func sortedKeys(m map[string]string) []string {
-	s := make([]string, len(m))
+	sorted := make([]string, len(m))
 	i := 0
+
 	for k := range m {
-		s[i] = k
+		sorted[i] = k
 		i++
 	}
-	sort.Strings(s)
-	return s
+
+	sort.Strings(sorted)
+
+	return sorted
 }
 
 func joinPlatforms(p []ocispecs.Platform) string {
@@ -102,5 +124,6 @@ func joinPlatforms(p []ocispecs.Platform) string {
 	for _, pp := range p {
 		str = append(str, platforms.Format(platforms.Normalize(pp)))
 	}
+
 	return strings.Join(str, ",")
 }
--- builder/commands/wrap.go
+++ builder/commands/wrap.go
@@ -9,12 +9,14 @@ import (
 
 func getClient(node *builder.Node) (*client.Client, error) {
 	opts := []client.ClientOpt{client.WithFailFast()}
+	//nolint:godox
 	// TODO: investigate tracing here
 	// client.WithTracerProvider, client.WithTracerDelegate
 	if node.CACert != "" || node.Cert != "" || node.Key != "" {
 		opts = append(opts, client.WithCredentials(node.Address.Host, node.CACert, node.Cert, node.Key))
 	}
-	ctx, cancel := context.WithTimeout(context.TODO(), node.ConnectionTimeout)
+
+	ctx, cancel := context.WithTimeout(context.Background(), node.ConnectionTimeout)
 	defer cancel()
 
 	return client.New(ctx, node.Address.String(), opts...)
