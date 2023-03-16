fa80474e0ecc77bf3cc880da454bd4d882a584e3
spacedub <spaceraccoon@codecomet.io>
2023-03-15 23:08:18 -0700

    work

--- machine/machine.go
+++ machine/machine.go
@@ -1,19 +1,19 @@
 package machine
 
 import (
+	"fmt"
 	"net/url"
 	"os"
 	"path/filepath"
 
-	// XXX Need to rip these out somehow and get them here.
 	"github.com/codecomet-io/isovaline/isovaline/config"
-	lima_cli "github.com/codecomet-io/isovaline/upstream/lima-cli"
 )
 
 func GetSocket() *url.URL {
 	sock := os.Getenv("_UNSTABLE_CODECOMET_CUSTOM_BUILDER_SOCKET")
 	if sock == "" {
-		sock = lima_cli.New(filepath.Join(config.Get().GetRunRoot(), "vm"), "runner").GetSock()
+		// sock = lima_cli.New(filepath.Join(config.Get().GetRunRoot(), "vm"), "runner").GetSock()
+		sock = fmt.Sprintf("unix://%s/%s/sock/buildkitd.sock", filepath.Join(config.Get().GetRunRoot(), "vm"), "runner")
 	}
 
 	u, _ := url.Parse(sock)
